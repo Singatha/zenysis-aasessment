3c3e94f6cd0f63fc60547f505ae014588ee92b5e
Singatha <xhantisingatha@gmail.com>
2022-05-03 14:02:20 +0200

    added api call to the frontend

---
+++ frontend/src/components/AlertDismis.js
@@ -0,0 +1,14 @@
+import Alert from 'react-bootstrap/Alert';
+
+function AlertDismis(props) {
+    return (
+        <Alert variant="danger" dismissible>
+          <Alert.Heading>Error</Alert.Heading>
+          <p>
+            {props.error}
+          </p>
+        </Alert>
+    );
+}
+
+export default AlertDismis;
